4387454ae5160dd5f1210a6eee777642108deba3
lalit-champ <lalitmalidz007@gmail.com>
2025-08-12 10:31:54 +0530

    Delete backend directory

--- backend/routes/authroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { register, login } = require('../controllers/authController');
-
-// Routes for authentication
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
